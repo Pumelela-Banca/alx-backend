aecdf4e274f48f5169958d6fcaad573aac6e00dc
Pumelela-Banca <pumelelabanca@gmail.com>
2024-01-31 16:35:04 +0200

    implemented get and put class

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Has a class called basic cache that stores
+basic information
+"""
+BasicCaching = __import__('base_caching').BasicCaching
+
+
+class BasicCache(BasicCaching):
+    """
+    Inherits from BasicCaching and implements put and get
+    """
+
+    def put(self, key, item):
+        """
+        Put objects into self.cache_data
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Gets key from self.cache_data
+        """
+        if key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
