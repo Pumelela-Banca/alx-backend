7328cd707f97a805a524d638a645b7511f3cc75c
Pumelela-Banca <pumelelabanca@gmail.com>
2024-01-31 18:54:28 +0200

    added doc string and class structure

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 """
-
+Implementation of LIFO
 """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    class with lifo caching
+    """
+
--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 """
-
+Implementation of LRUCache
 """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+
+    """
+    
