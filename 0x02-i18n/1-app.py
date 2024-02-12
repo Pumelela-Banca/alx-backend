ea24ac605f5ff9b4f6deff0f733fce22abf43758
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-13 00:29:59 +0200

    added al Q-2

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Flask start app and add babel
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Configures languages
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    """
+    renders index.html
+    """
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
