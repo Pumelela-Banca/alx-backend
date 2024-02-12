81674a7d404fb00a39bc54950edc8e89db31179f
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-13 00:37:07 +0200

    added get_locale to 2-app

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Flask start app and add babel
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Configures languages
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    """
+    renders index.html
+    """
+    return render_template('index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Gets locale for page
+    """
+    return (
+        request.accept_languages.best_match(app.config["LANGUAGES"]))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
