ea238a011e1070c5b9f861a08aee865b05040832
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-13 06:48:09 +0200

    removed test

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -36,7 +36,3 @@ def get_locale() -> str:
     if locale in Config.LANGUAGES:
         return locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
